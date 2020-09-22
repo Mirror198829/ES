6fb20e647b91e51e9e5006921396171c092f0e8e
caojing <412366756@qq.com>
2020-09-22 16:11:32 +0800

    新增vue中应用的示例

--- src/views/vue.vue
+++ src/views/vue.vue
@@ -16,10 +16,23 @@ export default {
 
   },
   methods:{
-
+    asycnFn(time){
+      return new Promise(resovle =>{
+        setTimeout(()=>{
+          console.log(`${time / 1000}秒之后，asycnFn执行中`)
+          resovle('asycnFn执行完成')
+        },time)
+      })
+    },
+    async init(){
+      console.log('准备执行asycnFn方法')
+      let res = await this.asycnFn(5000)
+      console.log(res)
+      console.log('我等await执行完再显示')
+    }
   },
   mounted(){
-
+    this.init()
   },
   created(){}
 }
